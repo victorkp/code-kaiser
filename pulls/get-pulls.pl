fd1e6a499d700eda50f918898317591a39d38fb3
Victor Kaiser-Pendergrast <v.kaiser.pendergrast@gmail.com>
2016-07-16 12:59:15 -0400

    Fixed string interpret in get-pulls

--- pulls/get-pulls.pl
+++ pulls/get-pulls.pl
@@ -6,10 +6,10 @@ my $url = "https://patch-diff.githubusercontent.com/raw/StephenBlackWasAlreadyTa
 
 # Get the last 200 pull requests
 for(my $i = 355; $i > 0; $i--) {
-    system "wget ${url}${i}.diff";
+    system "wget ${url}${i}.diff > /dev/null 2>&1";
 }
 
 system "rm -f wget.log";
 
 # Remove not-merged pulls
-system "grep -lrIZ --include \*.diff DOCTYPE . | xargs -0 rm -f --"
+system 'grep -lrIZ --include \*.diff DOCTYPE . | xargs -0 rm -f --'
